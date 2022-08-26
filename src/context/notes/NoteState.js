fc3cc1a0329e374b8557a3ef09557258853b7a1a
Abhinav Shukla <abhinavshukla693@gmail.com>
2022-08-26 22:46:42 +0530

    Created context API for notes

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,28 @@
+import NoteContext  from "./noteContext"
+import { useState } from "react"
+
+const NoteState = (props) =>{
+    const s1 = {
+        "name" : "Abhinav",
+        "class" : "6B"
+    }
+
+    const [state, setState] = useState(s1)
+    const update = () => {
+        setTimeout(() => {
+            setState({
+                "name" : "Eklavya",
+                "class" : "11A "
+            })
+        }, 1000)
+    }
+
+    
+    return (
+        <NoteContext.Provider value={{state, update}}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+export default NoteState;
