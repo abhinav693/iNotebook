b732388045572f7374b433a3feb002258e644415
Abhinav Shukla <abhinavshukla693@gmail.com>
2022-08-27 19:12:28 +0530

    Added Note card

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,10 @@
 import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
 
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(() => {
-    a.update()
-  })
+
   return (
     <div>
-      About {a.state.name} of class {a.state.class}
+
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,25 +1,96 @@
 import NoteContext  from "./noteContext"
 import { useState } from "react"
 
-const NoteState = (props) =>{
-    const s1 = {
-        "name" : "Abhinav",
-        "class" : "6B"
-    }
+const NoteState = (props) =>{    
+    const notesInitial = [
+        {
+          "_id": "630871b8e6880f2f825450f5",
+          "user": "630617100c10507ee12b4ec4",
+          "title": "New Note",
+          "description": "Wake up Sid",
+          "tag": "kbhi bhi",
+          "date": "2022-08-26T07:09:44.739Z",
+          "__v": 0
+        },
+        {
+          "_id": "6309b250dc9a3f251a8c61ae",
+          "user": "630617100c10507ee12b4ec4",
+          "title": "First Note",
+          "description": "Wake up kid",
+          "tag": "kuch bhi kaise bhai",
+          "date": "2022-08-27T05:57:36.203Z",
+          "__v": 0
+        },
+        {
+            "_id": "630871b8e6880f2f825450f5",
+            "user": "630617100c10507ee12b4ec4",
+            "title": "New Note",
+            "description": "Wake up Sid",
+            "tag": "kbhi bhi",
+            "date": "2022-08-26T07:09:44.739Z",
+            "__v": 0
+          },
+          {
+            "_id": "630871b8e6880f2f825450f5",
+            "user": "630617100c10507ee12b4ec4",
+            "title": "New Note",
+            "description": "Wake up Sid",
+            "tag": "kbhi bhi",
+            "date": "2022-08-26T07:09:44.739Z",
+            "__v": 0
+          },
+          {
+            "_id": "630871b8e6880f2f825450f5",
+            "user": "630617100c10507ee12b4ec4",
+            "title": "New Note",
+            "description": "Wake up Sid",
+            "tag": "kbhi bhi",
+            "date": "2022-08-26T07:09:44.739Z",
+            "__v": 0
+          },
+          {
+            "_id": "630871b8e6880f2f825450f5",
+            "user": "630617100c10507ee12b4ec4",
+            "title": "New Note",
+            "description": "Wake up Sid",
+            "tag": "kbhi bhi",
+            "date": "2022-08-26T07:09:44.739Z",
+            "__v": 0
+          },
+          {
+            "_id": "630871b8e6880f2f825450f5",
+            "user": "630617100c10507ee12b4ec4",
+            "title": "New Note",
+            "description": "Wake up Sid",
+            "tag": "kbhi bhi",
+            "date": "2022-08-26T07:09:44.739Z",
+            "__v": 0
+          },
+          {
+            "_id": "630871b8e6880f2f825450f5",
+            "user": "630617100c10507ee12b4ec4",
+            "title": "New Note",
+            "description": "Wake up Sid",
+            "tag": "kbhi bhi",
+            "date": "2022-08-26T07:09:44.739Z",
+            "__v": 0
+          },
+          {
+            "_id": "630871b8e6880f2f825450f5",
+            "user": "630617100c10507ee12b4ec4",
+            "title": "New Note",
+            "description": "Wake up Sid",
+            "tag": "kbhi bhi",
+            "date": "2022-08-26T07:09:44.739Z",
+            "__v": 0
+          }
+      ]
+      
+      const [notes, setNotes] = useState(notesInitial)
 
-    const [state, setState] = useState(s1)
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name" : "Eklavya",
-                "class" : "11A "
-            })
-        }, 1000)
-    }
 
-    
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
